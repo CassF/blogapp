271648995017a9718e67046028c8057953b8634b
CassF <cassandrafrimpong@gmail.com>
2016-12-16 19:33:15 +0000

    added the correct http verbs to the routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 //These are my own modules that require a relative path to find them (As they are not included in the node_modules folder. And nor should they be)
 const adminController = require("../controllers/adminController");
 
-//We are calling a function on router in this case .get(), which is the HTTP verb, and that takes 2 arguments.
+//We are calling a functions on router in this case .post() and .delete(), which are the HTTP verb, and that takes 2 arguments.
 //1 is the path (from the url), the 2nd is the function to call
-router.get("/create", adminController.createBlog);
-router.get("/delete", adminController.deleteBlog);
+router.post("/create", adminController.createBlog);
+router.delete("/delete", adminController.deleteBlog);
 
 //This just exports the router object so that it can be used elsewhere
 module.exports = router;
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //These are my own modules that require a relative path to find them (As they are not included in the node_modules folder. And nor should they be)
 const blogController = require("../controllers/blogController");
 
-//We are calling a function on router in this case .get(), which is the HTTP verb, and that takes 2 arguments.
+//We are calling two functions on router in this case they are both .get(), which is the HTTP verb, and that takes 2 arguments.
 //1 is the path (from the url), the 2nd is the function to call
 router.get("/blogs", blogController.viewBlog);
 router.get("/blogs/:id", blogController.viewBlog);
