75c7c621560f1111955a990ff6ba5189a6b0ac9d
CassF <cassandrafrimpong@gmail.com>
2016-12-21 12:21:19 +0000

    added chai

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,6 @@
-// const chai = require('chai');
-// const should = chai.should();
-// const expect = require('chai').expect;
-// const insert = require('chai').assert;
+const chai = require('chai');
+const should = chai.should();
+const expect = require('chai').expect;
+const insert = require('chai').assert;
 // const Functions = require("../functions");
 // const func = new Functions();
